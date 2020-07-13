9cab79c9279625e769db7bfe0083c0f4d4872e19
IbrahimMajdi <ibrmaj1@gmail.com>
2020-07-13 23:28:57 +0300

    lab12_02

--- seed.sql
+++ seed.sql
@@ -1,1 +1,12 @@
 Drop TABLE IF EXISTS books;
+
+Create TABLE books(
+id SERIAL PRIMARY KEY,
+
+title VARCHAR(255),
+authorName VARCHAR(255),
+description TEXT,
+thumbnail VARCHAR(255),
+shelfName VARCHAR(255),
+identifier VARCHAR(255)
+);
